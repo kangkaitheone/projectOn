19852b881576f91115bb1db1deb275ffc8f1399e
xing123peng <2223975037@qq.com>
2018-07-26 17:17:27 +0800

    login coding

--- src/main/java/com/lanou/Controller/UserController.java
+++ src/main/java/com/lanou/Controller/UserController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class UserController {
+    // 2018年07月26日17:17:03
     @RequestMapping(value = "login")
     public String login(){
         return "login";
